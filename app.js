f238ac4c83654b65349fd320adf4dce5da4eedc6
dacardenasa <diego.cardenasaleg@gmail.com>
2020-08-11 18:48:26 -0500

    Apply format documents to app.js

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
-const bcrypt = require('bcrypt');
-const User = require('./models/user');
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const bcrypt = require("bcrypt");
+const User = require("./models/user");
 const app = express();
 const port = 3000;
-require('dotenv').config();
+require("dotenv").config();
 
-// Set config template engine pug 
-app.set('view engine', 'pug');
-app.set('views', './public/views');
+// Set config template engine pug
+app.set("view engine", "pug");
+app.set("views", "./public/views");
 
-// Use config to consume static resource files 
-app.use(express.static('public'));
+// Use config to consume static resource files
+app.use(express.static("public"));
 
 // Use config to get forms data request
 app.use(express.urlencoded({ extended: true }));
@@ -20,21 +20,21 @@ app.use(express.urlencoded({ extended: true }));
 // Use config to use cookies to save session
 app.use(
   cookieSession({
-    secret: 'lorem ipsum dolor',
+    secret: "lorem ipsum dolor",
     maxAge: 24 * 60 * 60 * 1000,
   })
 );
 
 // Handle DB conection
+
 mongoose.connect(
   process.env.MONGODB_URL || "mongodb://localhost:27017/mongo-1",
-  {
-    useNewUrlParser: true,
-  }
+  { useNewUrlParser: true }
 );
-mongoose.connection.on('error', (error) => console.error(error.message));
 
-// Middleware to handle start session into selected routes 
+mongoose.connection.on("error", (error) => console.error(error.message));
+
+// Middleware to handle start session into selected routes
 const requireUser = async (req, res, next) => {
   const userId = req.session.userId;
   if (userId) {
@@ -42,21 +42,21 @@ const requireUser = async (req, res, next) => {
     res.locals.user = user;
     next();
   } else {
-    return res.redirect('/login');
+    return res.redirect("/login");
   }
 };
 
-// App routes 
-app.get('/', requireUser, async (req, res) => {
+// App routes
+app.get("/", requireUser, async (req, res) => {
   const users = await User.find();
-  res.render('index', { users: users });
+  res.render("index", { users: users });
 });
 
-app.get('/register', requireUser, (req, res) => {
-  res.render('form');
+app.get("/register", requireUser, (req, res) => {
+  res.render("form");
 });
 
-app.post('/register', (req, res) => {
+app.post("/register", (req, res) => {
   const name = req.body.name;
   const email = req.body.email;
   const password = req.body.password;
@@ -68,36 +68,36 @@ app.post('/register', (req, res) => {
     });
     await user.save((error) => {
       if (error) return console.error(error.message);
-      return res.redirect('/');
+      return res.redirect("/");
     });
   });
 });
 
-app.get('/login', (req, res) => {
-  res.render('login');
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   const user = await User.findOne({ email: email });
   if (user) {
     bcrypt.compare(password, user.password).then((result) => {
       if (!result)
-        return res.render('login', {
-          error: 'Password doesn\'t match. Try again!',
+        return res.render("login", {
+          error: "Password doesn't match. Try again!",
         });
       req.session.userId = user._id;
-      return res.redirect('/');
+      return res.redirect("/");
     });
   } else {
-    res.render('login', { error: 'Email doesn\'t exist. Try again!' });
+    res.render("login", { error: "Email doesn't exist. Try again!" });
   }
 });
 
-app.get('/logout', (req, res) => {
+app.get("/logout", (req, res) => {
   req.session = null;
-  return res.render('login');
+  return res.render("login");
 });
 
 // Deploy NodeJS Server
